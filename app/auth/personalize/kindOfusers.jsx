b797694f17cbf2bffcd280dfa9df9ad6fcb8ac66
chawanakorns <chawanakorn_s@cmu.ac.th>
2025-10-24 14:17:08 +0700

    Updated personalized questionnaires pages

--- app/auth/personalize/kindOfusers.jsx
+++ app/auth/personalize/kindOfusers.jsx
@@ -111,18 +111,18 @@ export default function KindOfUsers() {
 
 const CARD_SIZE = (Dimensions.get("window").width - 70) / 2;
 const styles = StyleSheet.create({
-  container: { padding: 25, paddingTop: 50, backgroundColor: Colors.WHITE, flex: 1 },
-  title: { fontSize: 28, color: Colors.PRIMARY, fontFamily: "outfit-bold", textAlign: "center", marginTop: 80 },
-  subtitle: { fontSize: 14, color: Colors.PRIMARY, fontFamily: "outfit", textAlign: "center", marginTop: 10 },
-  question: { fontSize: 16, color: Colors.PRIMARY, fontFamily: "outfit-bold", marginTop: 30 },
+  container: { padding: 25, paddingTop: 50, backgroundColor: '#7E9DFF', flex: 1 },
+  title: { fontSize: 32, color: '#FFFFFF', fontFamily: "outfit-bold", textAlign: "center", marginTop: 80 },
+  subtitle: { fontSize: 18, color: '#FFFFFF', fontFamily: "outfit", textAlign: "center", marginTop: 10 },
+  question: { fontSize: 20, color: '#FFFFFF', fontFamily: "outfit-bold", marginTop: 30 },
   card: { width: CARD_SIZE, height: 130, marginVertical: 10, borderRadius: 15, overflow: "hidden", backgroundColor: Colors.GRAY, justifyContent: 'center', alignItems: 'center' },
   selectedCard: { borderWidth: 2, borderColor: '#FFC107' },
   image: { width: "100%", height: "100%", position: "absolute" },
   overlay: { flex: 1, ...StyleSheet.absoluteFillObject, backgroundColor: "rgba(0,0,0,0.4)", justifyContent: "center", alignItems: "center" },
-  label: { color: Colors.WHITE, fontFamily: "outfit-bold", fontSize: 16 },
-  footer: { marginTop: 'auto', paddingTop: 10 },
+  label: { color: '#FFFFFF', fontFamily: "outfit-bold", fontSize: 16 },
+  footer: { marginTop: 20, paddingTop: 10, marginBottom: 40},
   button: { padding: 15, borderRadius: 15, marginTop: 10, borderWidth: 1 },
-  primaryButton: { borderColor: Colors.BLUE, backgroundColor: Colors.BLUE },
-  secondaryButton: { backgroundColor: Colors.WHITE },
-  buttonText: { fontFamily: "outfit", fontSize: 16, color: Colors.WHITE, textAlign: "center" },
+  primaryButton: { borderColor: '#000000ff', backgroundColor: '#000000ff' },
+  secondaryButton: { backgroundColor: '#FFFFFF' },
+  buttonText: { fontFamily: "outfit", fontSize: 16, color: '#FFFFFF', textAlign: "center" },
 });
